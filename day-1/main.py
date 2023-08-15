5b908572ec027f501d8a96a633e5f1e79d6033fc
Antonio Netto Dev <antonionettodev@gmail.com>
2023-08-15 16:44:36 -0300

    Dia 1 do Desafio

---
+++ day-1/main.py
@@ -0,0 +1,4 @@
+print('Bem vindo ao gerador de nomes para bandas')
+city = input('Digite o nome da cidade que você nasceu: ')
+pet = input('Digite o nome do seu animal de estimação: ')
+print(f'O nome da sua banda pode ser {city} {pet}')
